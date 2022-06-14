3aaefef5fdd29ba949f48b3778842e72448a90e2
Stevani Doris <70757128+stevanidoris@users.noreply.github.com>
2022-06-14 17:24:28 +0700

    Create DetailProduct.jsx
    tambah halaman detailproduct

---
+++ src/components/DetailProduct.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DetailProduct = () => {
+    return (
+        <div className="bg-white">
+            <div className="flex space-x-4">
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
+        </div>
+    );
+};
+
+export default DetailProduct;

50603fdaa9fd5ae19f1407e6ae44291dbb776aee
Stevani Doris <70757128+stevanidoris@users.noreply.github.com>
2022-06-14 22:07:06 +0700

    tambah fitur detailproduct

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import DetailProduct from "./components/DetailProduct";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navbar />} />
+                <Route path="/" element={
+                <>
+                <Navbar />
+                <DetailProduct/>
+                </>
+                } 
+                />
+                
             </Routes>
         </BrowserRouter>
     );

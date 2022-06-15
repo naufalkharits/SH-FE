cab0c37ba40681fcdd3b3ddb56bda41568c5bfea
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-06-15 19:02:28 +0700

    Carousel fixed
    but, still need improvement.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,67 @@
+import { useState } from "react";
 import Slider from "react-slick";
+import { FiArrowRight, FiArrowLeft } from "react-icons/fi";
+import banner1 from "../images/banner-1.png";
+import banner2 from "../images/banner-2.png";
+import banner3 from "../images/banner-3.png";
+import banner4 from "../images/banner-4.png";
 
 const Hero = () => {
+    const images = [banner1, banner2, banner3, banner4];
+
+    // const NextArrow = ({ onClick }) => (
+    //     <div
+    //         className="absolute right-0 top-1/2 z-10 cursor-pointer bg-black"
+    //         onClick={onClick}
+    //     >
+    //         <FiArrowRight className="text-white" />
+    //     </div>
+    // );
+
+    // const PrevArrow = ({ onClick }) => (
+    //     <div
+    //         className="absolute left-0 top-1/2 z-10 cursor-pointer bg-black"
+    //         onClick={onClick}
+    //     >
+    //         <FiArrowLeft className="text-white" />
+    //     </div>
+    // );
+
+    const [imageIndex, setImageIndex] = useState(0);
+
     const settings = {
-        dots: true,
+        centerMode: true,
         infinite: true,
+        centerPadding: 0,
+        slidesToShow: 3,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+        // nextArrow: <NextArrow />,
+        // prevArrow: <PrevArrow />,
+        beforeChange: (current, next) => setImageIndex(next),
     };
 
     return (
-        <div>
-            <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
+        <Slider {...settings}>
+            {images.map((img, idx) => (
+                <div
+                    className={
+                        idx === imageIndex
+                            ? "opacity-100 transition-transform"
+                            : "scale-[0.75] opacity-75 transition-transform"
+                    }
+                >
+                    <img className="mx-auto w-full" src={img} alt={img} />
                 </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </Slider>
-        </div>
+            ))}
+        </Slider>
     );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
 import JualButton from "../components/JualButton";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
     return (
         <>
+            <Hero />
             <div className="container my-8 mx-auto flex flex-wrap justify-center gap-4 px-4">
                 <ProductCard />
                 <ProductCard />

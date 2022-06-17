bdc84b5a19e222975916e162cad99a5e93b9c65c
Stevani Doris <70757128+stevanidoris@users.noreply.github.com>
2022-06-17 14:45:49 +0700

    Fixing InfoProfil

--- src/components/CategorySeller.jsx
+++ src/components/CategorySeller.jsx
@@ -1,26 +1,26 @@
-// import { ScrollingCarousel } from "@trendyol-js/react-carousel";
-// import React from "react";
-// import { FiBox, FiDollarSign, FiHeart } from "react-icons/fi";
+import { ScrollingCarousel } from "@trendyol-js/react-carousel";
+import React from "react";
+import { FiBox, FiDollarSign, FiHeart } from "react-icons/fi";
 
-// const CategorySeller = () => {
-//     return (
-//         <div className="sm:hidden">
-//             <ScrollingCarousel leftIcon={"<"} rightIcon={">"}>
-//                 <div className="flex items-center gap-2 rounded-xl bg-primary-purple-04 py-3 px-6 text-white">
-//                     <FiBox />
-//                     <span>Semua Produk</span>
-//                 </div>
-//                 <div className="ml-4 flex items-center gap-2 rounded-xl bg-primary-purple-01 py-3 px-6">
-//                     <FiHeart />
-//                     <span>Diminati</span>
-//                 </div>
-//                 <div className="ml-4 flex items-center gap-2 rounded-xl bg-primary-purple-01 py-3 px-6">
-//                     <FiDollarSign />
-//                     <span>Terjual</span>
-//                 </div>
-//             </ScrollingCarousel>
-//         </div>
-//     );
-// };
+const CategorySeller = () => {
+    return (
+        <div className="sm:hidden">
+            <ScrollingCarousel leftIcon={"<"} rightIcon={">"}>
+                <div className="flex items-center gap-2 rounded-xl bg-primary-purple-04 py-3 px-6 text-white">
+                    <FiBox />
+                    <span>Semua Produk</span>
+                </div>
+                <div className="ml-4 flex items-center gap-2 rounded-xl bg-primary-purple-01 py-3 px-6">
+                    <FiHeart />
+                    <span>Diminati</span>
+                </div>
+                <div className="ml-4 flex items-center gap-2 rounded-xl bg-primary-purple-01 py-3 px-6">
+                    <FiDollarSign />
+                    <span>Terjual</span>
+                </div>
+            </ScrollingCarousel>
+        </div>
+    );
+};
 
-// export default CategorySeller;
+export default CategorySeller;

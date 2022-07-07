5ee8dba972b06b2a2abb29f1486ca636ebd692bb
Stevani Doris <70757128+stevanidoris@users.noreply.github.com>
2022-07-07 18:43:22 +0700

    Fixing Skeleton Product

--- src/components/skeletons/ProductSkeleton.jsx
+++ src/components/skeletons/ProductSkeleton.jsx
@@ -1,7 +1,6 @@
 const ProductSkeleton = () => {
     return (
         <>
-            {/* <div className="h-72 w-72 animate-pulse rounded-xl bg-gray"></div> */}
             <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
             <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
             <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>

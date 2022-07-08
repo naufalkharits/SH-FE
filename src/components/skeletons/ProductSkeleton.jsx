c4f96e5af892e6f96b9bfca114d2aed9f64a6ffb
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-08 16:51:41 +0700

    Home Skeleton Enhancement

--- src/components/skeletons/ProductSkeleton.jsx
+++ src/components/skeletons/ProductSkeleton.jsx
@@ -1,16 +1,86 @@
 const ProductSkeleton = () => {
     return (
         <>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
-            <div className="ml-4 mb-4 h-72 w-72 animate-pulse rounded-xl bg-gray"></div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
+            <div className="w-1/2 p-4 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                    <div className="h-48 w-full animate-pulse rounded bg-gray" />
+                    <div className="h-3 w-24 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-2 w-12 animate-pulse rounded-md bg-gray"></div>
+                    <div className="h-4 w-20 animate-pulse rounded-md bg-gray"></div>
+                </div>
+            </div>
         </>
     );
 };

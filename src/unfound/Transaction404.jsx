93c90754a1fbbb2ee192026458d924ec7e0aa192
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-12 21:09:18 +0700

    Minor Fixes

--- src/unfound/Transaction404.jsx
+++ src/unfound/Transaction404.jsx
@@ -1,7 +1,7 @@
 import terjual_404 from "../images/terjual-404.png";
 
 const Transaction404 = () => {
-    return <img src={terjual_404} alt="" />;
+    return <img className="mx-auto" src={terjual_404} alt="" />;
 };
 
 export default Transaction404;

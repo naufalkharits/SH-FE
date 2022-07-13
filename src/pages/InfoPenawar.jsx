fd1e38fae0990901db3af67b7091f34e83135b8b
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-13 16:31:58 +0700

    InfoPenawar Enhancements

--- src/pages/InfoPenawar.jsx
+++ src/pages/InfoPenawar.jsx
@@ -1,21 +1,22 @@
-import { React, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FiArrowLeft } from "react-icons/fi";
-import ModalBerhasil from "../components/modals/ModalBerhasil";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { FiArrowLeft } from "react-icons/fi";
+import { me } from "../redux/authSlice";
+import ModalBerhasil from "../components/modals/ModalBerhasil";
 import TransactionCard from "../components/TransactionCard";
-import orang from "../images/orang.png";
+import ProfileCard from "../components/ProfileCard";
 
 const InfoPenawar = () => {
-    const dispatch = useDispatch();
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { decodedAccess } = useSelector((state) => state.auth);
     const [modalOn, setModalOn] = useState(false);
     const [choice, setChoice] = useState(false);
 
-    const clicked = () => {
-        setModalOn(true);
-    };
+    useEffect(() => {
+        decodedAccess && dispatch(me(decodedAccess?.id));
+    }, [decodedAccess, dispatch]);
 
     return (
         <div className="mx-auto mt-4 flex w-full justify-between sm:mt-10 md:w-full lg:w-[1024px]">
@@ -28,43 +29,9 @@ const InfoPenawar = () => {
                 />
             </div>
             <div className="w-full space-y-7 px-4">
-                <div className="flex items-center gap-4 rounded-2xl p-4 shadow">
-                    <img src={orang} alt="" />
-                    <div className="space-y-1">
-                        <div>Nama Pembeli</div>
-                        <div className="text-xs text-neutral-03">Kota</div>
-                    </div>
-                </div>
-                <p className="font-medium">Daftar Produkmu yang Ditawar</p>
+                <ProfileCard />
+                <div className="font-medium">Daftar Produkmu yang Ditawar</div>
                 <TransactionCard />
-
-                {/* <div className="flex gap-6 rounded-xl">
-                    <img
-                        src="/img/jam-2.png"
-                        alt=""
-                        className="h-14 w-14 rounded-xl object-cover"
-                    />
-                    <div className="w-full space-y-1">
-                        <div className="flex justify-between text-xs text-neutral-03">
-                            <p>Penawaran Produk</p>
-                            <p>20 Apr, 14:04</p>
-                        </div>
-                        <p className="">Jam Tangan Casio</p>
-                        <p className="">Rp 250.000</p>
-                        <p className="">Ditawar Rp 200.000</p>
-                        <div className="flex justify-end">
-                            <button className="mr-4 w-1/3 rounded-2xl border border-primary-purple-04 py-2">
-                                Tolak
-                            </button>
-                            <button
-                                onClick={clicked}
-                                className="w-1/3 rounded-2xl bg-primary-purple-04 py-2 text-white"
-                            >
-                                Terima
-                            </button>
-                        </div>
-                    </div>
-                </div> */}
             </div>
             <div className="hidden h-[30px] w-[30px] sm:ml-10 sm:mr-10 sm:block lg:ml-20"></div>
             {modalOn && (

89e8251936e9242c5785e46b01cc0ed076ea1293
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-14 21:32:03 +0700

    Minor Fix Skeleton Loading

--- src/components/AllProduct.jsx
+++ src/components/AllProduct.jsx
@@ -1,57 +1,54 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getFilteredProduct } from "../redux/productsSlice";
-import AddProductButton from "./buttons/AddProductButton";
-import ProductCard from "./ProductCard";
-import SellerSkeleton from "./skeletons/SellerSkeleton";
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getFilteredProduct } from "../redux/productsSlice"
+import AddProductButton from "./buttons/AddProductButton"
+import ProductCard from "./ProductCard"
+import SellerSkeleton from "./skeletons/SellerSkeleton"
 
 const AllProduct = () => {
-    const dispatch = useDispatch();
-    const { profile } = useSelector((state) => state.auth);
-    const { filteredProduct, loading } = useSelector((state) => state.products);
+    const dispatch = useDispatch()
+    const { profile } = useSelector((state) => state.auth)
+    const loadingAuth = useSelector((state) => state.auth.loading)
+    const { filteredProduct, loading } = useSelector((state) => state.products)
 
-    const [id, setId] = useState(null);
+    const [id, setId] = useState(null)
 
     useEffect(() => {
-        profile && setId(profile.id);
-        id && dispatch(getFilteredProduct(id));
-    }, [profile, id, dispatch]);
+        profile && setId(profile.id)
+        id && dispatch(getFilteredProduct(id))
+    }, [profile, id, dispatch])
 
     return (
         <>
-            {profile && (
-                <>
-                    {loading === "pending" ? (
-                        <SellerSkeleton />
-                    ) : (
-                        filteredProduct && (
-                            <>
-                                {filteredProduct.length === 0 ? (
-                                    <AddProductButton height={"h-32"} />
-                                ) : (
-                                    <AddProductButton height={"h-full"} />
-                                )}
-                                {filteredProduct.map((product) => (
-                                    <div
-                                        className="w-1/2 p-4 lg:w-1/3 2xl:w-1/4"
-                                        key={product.id}
-                                    >
-                                        <ProductCard
-                                            id={product.id}
-                                            name={product.name}
-                                            price={product.price}
-                                            category={product.category}
-                                            pictures={product.pictures[0]}
-                                        />
-                                    </div>
-                                ))}
-                            </>
-                        )
-                    )}
-                </>
+            {loadingAuth === "pending" || loading === "pending" ? (
+                <SellerSkeleton />
+            ) : (
+                filteredProduct && (
+                    <>
+                        {filteredProduct.length === 0 ? (
+                            <AddProductButton height={"h-32"} />
+                        ) : (
+                            <AddProductButton height={"h-full"} />
+                        )}
+                        {filteredProduct.map((product) => (
+                            <div
+                                className="w-1/2 p-4 lg:w-1/3 2xl:w-1/4"
+                                key={product.id}
+                            >
+                                <ProductCard
+                                    id={product.id}
+                                    name={product.name}
+                                    price={product.price}
+                                    category={product.category}
+                                    pictures={product.pictures[0]}
+                                />
+                            </div>
+                        ))}
+                    </>
+                )
             )}
         </>
-    );
-};
+    )
+}
 
-export default AllProduct;
+export default AllProduct

13306af72b72ae1476d36b6babf550c76aa49fd7
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-15 22:29:28 +0700

    Add Text

--- src/components/TitlePerPage.jsx
+++ src/components/TitlePerPage.jsx
@@ -1,15 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom"
 
 const TitlePerPage = () => {
-    const location = useLocation();
+    const location = useLocation()
 
     return (
         <h1 className="text-xl font-bold sm:hidden">
+            {location.pathname.includes("/user") && "Profile"}
             {location.pathname.includes("/notification") && "Notifikasi"}
             {location.pathname.includes("/manage-product") &&
                 "Daftar Jual Saya"}
         </h1>
-    );
-};
+    )
+}
 
-export default TitlePerPage;
+export default TitlePerPage

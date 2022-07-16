5b3a0a2d846121edd0ecdb6688b3fd7d33167ce0
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-16 16:13:26 +0700

    Enhances Skeleton Loading

--- src/components/skeletons/SellerSkeleton.jsx
+++ src/components/skeletons/SellerSkeleton.jsx
@@ -2,7 +2,7 @@ const SellerSkeleton = () => {
     return (
         <>
             <div className="w-1/2 p-4 lg:w-1/3 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-12 animate-pulse rounded-md bg-gray"></div>
@@ -10,7 +10,7 @@ const SellerSkeleton = () => {
                 </div>
             </div>
             <div className="w-1/2 p-4 lg:w-1/3 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-12 animate-pulse rounded-md bg-gray"></div>
@@ -18,7 +18,7 @@ const SellerSkeleton = () => {
                 </div>
             </div>
             <div className="w-1/2 p-4 lg:w-1/3 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-12 animate-pulse rounded-md bg-gray"></div>
@@ -26,7 +26,7 @@ const SellerSkeleton = () => {
                 </div>
             </div>
             <div className="w-1/2 p-4 lg:w-1/3 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-12 animate-pulse rounded-md bg-gray"></div>
@@ -34,7 +34,7 @@ const SellerSkeleton = () => {
                 </div>
             </div>
         </>
-    );
-};
+    )
+}
 
-export default SellerSkeleton;
+export default SellerSkeleton
--- src/components/skeletons/WishlistProfileSkeleton.jsx
+++ src/components/skeletons/WishlistProfileSkeleton.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-
 const WishlistProfileSkeleton = () => {
     return (
         <>
-            <div className="w-full sm:w-1/2 my-4 sm:mr-6 lg:w-1/3 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+            <div className="my-4 w-full sm:mr-6 sm:w-1/2 lg:w-1/3 2xl:w-1/4">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-16 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-6 w-20 animate-pulse rounded-md bg-gray"></div>
                 </div>
             </div>
-            <div className="hidden sm:block sm:w-1/2 my-4 lg:mr-6 lg:w-1/3 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+            <div className="my-4 hidden sm:block sm:w-1/2 lg:mr-6 lg:w-1/3 2xl:w-1/4">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-16 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-6 w-20 animate-pulse rounded-md bg-gray"></div>
                 </div>
             </div>
-            <div className="hidden my-4 lg:block lg:w-1/3 2xl:mr-6 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+            <div className="my-4 hidden lg:block lg:w-1/3 2xl:mr-6 2xl:w-1/4">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-16 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-6 w-20 animate-pulse rounded-md bg-gray"></div>
                 </div>
             </div>
-            <div className="hidden my-4 lg:w-1/3 2xl:block 2xl:w-1/4">
-                <div className="space-y-2 rounded-md p-2 shadow-md ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
+            <div className="my-4 hidden lg:w-1/3 2xl:block 2xl:w-1/4">
+                <div className="space-y-2 rounded-md p-2 shadow ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10">
                     <div className="h-48 w-full animate-pulse rounded bg-gray" />
                     <div className="h-5 w-24 animate-pulse rounded-md bg-gray"></div>
                     <div className="h-4 w-16 animate-pulse rounded-md bg-gray"></div>
@@ -39,4 +37,4 @@ const WishlistProfileSkeleton = () => {
     )
 }
 
-export default WishlistProfileSkeleton;
+export default WishlistProfileSkeleton

aa107483758b5ceaa1a76e6254319d97630b7110
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-19 00:20:50 +0700

    Rename NegoButton to NegoFloatingButton

--- src/components/buttons/NegoFloatingButton.jsx
+++ src/components/buttons/NegoFloatingButton.jsx
@@ -1,4 +1,4 @@
-const NegoButton = () => {
+const NegoFloatingButton = () => {
     return (
         <button className="fixed inset-x-0 bottom-8 z-50 mx-auto w-fit rounded-2xl bg-primary-purple-04 px-6 py-3.5 text-white shadow-lg shadow-primary-purple-03 hover:bg-primary-purple-05 sm:hidden">
             <span>Saya Tertarik dan ingin Nego</span>
@@ -6,4 +6,4 @@ const NegoButton = () => {
     )
 }
 
-export default NegoButton
+export default NegoFloatingButton

66f8d4eacf278d991e9a7d9de87e155af5b7cce6
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-19 14:29:11 +0700

    Add Dark Mode on 404 Page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,21 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 const Error = () => {
     return (
         <div className="flex min-h-screen w-full flex-col items-center justify-center">
-            <div className="text-9xl font-extrabold tracking-widest">404</div>
+            <div className="text-9xl font-extrabold tracking-widest dark:text-white">
+                404
+            </div>
             <div className="absolute rotate-12 rounded bg-primary-purple-04 px-2 text-sm text-white">
                 Page Not Found
             </div>
 
             <Link
                 to="/"
-                className="text rounded-2xl border border-primary-purple-04 py-2 px-4 shadow-md hover:bg-primary-purple-05 hover:text-white"
+                className="text rounded-2xl border border-primary-purple-04 py-2 px-4 shadow-md hover:bg-primary-purple-05 hover:text-white dark:text-white"
             >
                 Go Home
             </Link>
         </div>
-    );
-};
+    )
+}
 
-export default Error;
+export default Error

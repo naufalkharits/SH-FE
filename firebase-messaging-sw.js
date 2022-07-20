fedb4602e870049d327e5bccd84c740626757d8b
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-20 20:22:42 +0700

    Create .eslintignore

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,8 @@
-// Scripts for firebase and firebase messaging
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js")
 importScripts(
     "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
 )
 
-// Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -18,8 +16,6 @@ firebase.initializeApp(firebaseConfig)
 const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage((payload) => {
-    console.log("Received background message ", payload)
-
     const notificationTitle = payload.notification.title
     const notificationOptions = {
         body: payload.notification.body,

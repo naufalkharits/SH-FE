e851d9af7d28c3a1b11edcbb2161f424887bfa5e
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-22 08:51:39 +0700

    Remove Chart Related

--- src/components/Sold.jsx
+++ src/components/Sold.jsx
@@ -1,278 +1,86 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Swiper, SwiperSlide } from "swiper/react"
 import dayjs from "dayjs"
-
 import { priceFormatter } from "../utils/priceFormatter"
 import { getTransaction, transactionSelectors } from "../redux/transactionSlice"
 import Transaction404 from "./unfound/Transaction404"
 import TransactionSkeleton from "./skeletons/TransactionSkeleton"
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-} from "chart.js"
-import { Line } from "react-chartjs-2"
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title)
-
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-        title: {
-            display: true,
-            text: "Chart.js Line Chart",
-        },
-    },
-}
-
-const userData = [
-    {
-        id: 1,
-        year: 2016,
-        pendapatanBersih: 80000,
-        produkTerjual: 823,
-    },
-    {
-        id: 2,
-        year: 2017,
-        pendapatanBersih: 45677,
-        produkTerjual: 345,
-    },
-    {
-        id: 3,
-        year: 2018,
-        pendapatanBersih: 78888,
-        produkTerjual: 555,
-    },
-    {
-        id: 4,
-        year: 2019,
-        pendapatanBersih: 90000,
-        produkTerjual: 4555,
-    },
-    {
-        id: 5,
-        year: 2020,
-        pendapatanBersih: 4300,
-        produkTerjual: 234,
-    },
-]
-
-const data = {
-    labels: userData.map((data) => data.year),
-    datasets: [
-        {
-            label: "Pendapatan Bersih",
-            data: userData.map((data) => data.pendapatanBersih),
-            borderColor: "rgb(255, 99, 132)",
-            backgroundColor: "rgba(255, 99, 132, 0.5)",
-        },
-        {
-            label: "Produk Terjual",
-            data: userData.map((data) => data.produkTerjual),
-            borderColor: "rgb(50, 162, 235)",
-            backgroundColor: "rgba(50, 162, 235, 0.5)",
-        },
-    ],
-}
 
 const Sold = () => {
     const dispatch = useDispatch()
-    const [status] = useState("")
-    const [as] = useState("seller")
 
     const transaction = useSelector(transactionSelectors.selectAll)
 
     const { loading } = useSelector((state) => state.transaction)
 
     useEffect(() => {
-        dispatch(getTransaction({ status, as }))
-    }, [status, as, dispatch])
+        dispatch(getTransaction({ status: "COMPLETED", as: "seller" }))
+    }, [dispatch])
+
+    console.log(transaction)
 
     return (
-        <>
-            <div className="mt-4 w-full space-y-5 px-5 sm:pl-10">
-                <p className="font-medium dark:text-white">
-                    Histori Penjualan Produk
-                </p>
-                {loading === "pending" ? (
-                    <TransactionSkeleton />
-                ) : transaction?.length === 0 ? (
-                    <div className="my-16 w-full">
-                        <Transaction404 />
-                    </div>
-                ) : (
-                    transaction?.map(
-                        (tx) =>
-                            tx.status === "COMPLETED" && (
-                                <div key={tx.id} className="w-full space-y-7">
-                                    <div className="flex gap-6 rounded-xl">
-                                        <Swiper className="h-14 w-16 rounded-xl object-cover">
-                                            {tx.product.pictures.map(
-                                                (picture) => (
-                                                    <SwiperSlide key={picture}>
-                                                        <img
-                                                            className="h-14 w-14 rounded-xl object-cover"
-                                                            src={picture}
-                                                            alt=""
-                                                        />
-                                                    </SwiperSlide>
-                                                )
-                                            )}
-                                        </Swiper>
-                                        <div className="w-full space-y-1">
-                                            <div className="flex justify-between text-xs text-neutral-03 dark:text-zinc-400">
-                                                <span>Produk Terjual</span>
-                                                <span>
-                                                    {dayjs(tx.updatedAt).format(
-                                                        "D MMM, HH:mm"
-                                                    )}
-                                                </span>
-                                            </div>
-                                            <div className="dark:text-white">
-                                                {tx?.product.name}
-                                            </div>
-                                            {tx?.product.price === tx?.price ? (
-                                                <div className="dark:text-white">
-                                                    Berhasil terjual{" "}
-                                                    {priceFormatter(
-                                                        tx?.product.price
-                                                    )}
-                                                </div>
-                                            ) : (
-                                                <>
-                                                    <div className="line-through dark:text-white">
-                                                        {priceFormatter(
-                                                            tx?.product.price
-                                                        )}
-                                                    </div>
-                                                    <div className="dark:text-white">
-                                                        Berhasil terjual{" "}
-                                                        {priceFormatter(
-                                                            tx?.price
-                                                        )}
-                                                    </div>
-                                                </>
-                                            )}
-                                        </div>
-                                    </div>
-                                    <div className="h-px bg-neutral-200 dark:bg-zinc-800"></div>
-                                </div>
-                            )
-                    )
-                )}
-            </div>
-            {/* <div className="w-full sm:p-4">
-                <div className="space-y-4 rounded-2xl border border-neutral-200 p-4 shadow-md">
-                    <div className="font-medium">Statisti Tokomu</div>
-                    <div className="flex items-center gap-4">
-                        <div className="rounded-md p-4 shadow">
-                            <div>Pendapatan Bersih Baru</div>
-                            <div>Rp0</div>
-                            <div>0% dari 7 hari terakhir</div>
-                        </div>
-                        <div className="rounded-md p-4 shadow">
-                            <div>Produk Terjual</div>
-                            <div>0</div>
-                            <div>0% dari 7 hari terakhir</div>
-                        </div>
-                    </div>
-                    <div className="h-48 w-full">
-                        <Line options={options} data={data} />
-                    </div>
-                    <table className="whitespace-no-wrap w-full table-auto text-left">
-                    <thead>
-                        <tr>
-                            <th className="title-font text-gray-900 bg-gray-100 rounded-tl rounded-bl px-4 py-3 text-sm font-medium tracking-wider">
-                                Plan
-                            </th>
-                            <th className="title-font text-gray-900 bg-gray-100 px-4 py-3 text-sm font-medium tracking-wider">
-                                Speed
-                            </th>
-                            <th className="title-font text-gray-900 bg-gray-100 px-4 py-3 text-sm font-medium tracking-wider">
-                                Storage
-                            </th>
-                            <th className="title-font text-gray-900 bg-gray-100 px-4 py-3 text-sm font-medium tracking-wider">
-                                Price
-                            </th>
-                            <th className="title-font text-gray-900 bg-gray-100 w-10 rounded-tr rounded-br text-sm font-medium tracking-wider"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="px-4 py-3">Start</td>
-                            <td className="px-4 py-3">5 Mb/s</td>
-                            <td className="px-4 py-3">15 GB</td>
-                            <td className="text-gray-900 px-4 py-3 text-lg">
-                                Free
-                            </td>
-                            <td className="w-10 text-center">
-                                <input name="plan" type="radio" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="border-gray-200 border-t-2 px-4 py-3">
-                                Pro
-                            </td>
-                            <td className="border-gray-200 border-t-2 px-4 py-3">
-                                25 Mb/s
-                            </td>
-                            <td className="border-gray-200 border-t-2 px-4 py-3">
-                                25 GB
-                            </td>
-                            <td className="border-gray-200 text-gray-900 border-t-2 px-4 py-3 text-lg">
-                                $24
-                            </td>
-                            <td className="border-gray-200 w-10 border-t-2 text-center">
-                                <input name="plan" type="radio" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="border-gray-200 border-t-2 px-4 py-3">
-                                Business
-                            </td>
-                            <td className="border-gray-200 border-t-2 px-4 py-3">
-                                36 Mb/s
-                            </td>
-                            <td className="border-gray-200 border-t-2 px-4 py-3">
-                                40 GB
-                            </td>
-                            <td className="border-gray-200 text-gray-900 border-t-2 px-4 py-3 text-lg">
-                                $50
-                            </td>
-                            <td className="border-gray-200 w-10 border-t-2 text-center">
-                                <input name="plan" type="radio" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="border-gray-200 border-t-2 border-b-2 px-4 py-3">
-                                Exclusive
-                            </td>
-                            <td className="border-gray-200 border-t-2 border-b-2 px-4 py-3">
-                                48 Mb/s
-                            </td>
-                            <td className="border-gray-200 border-t-2 border-b-2 px-4 py-3">
-                                120 GB
-                            </td>
-                            <td className="border-gray-200 text-gray-900 border-t-2 border-b-2 px-4 py-3 text-lg">
-                                $72
-                            </td>
-                            <td className="border-gray-200 w-10 border-t-2 border-b-2 text-center">
-                                <input name="plan" type="radio" />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div className="mt-4 w-full space-y-5 px-5 sm:pl-10">
+            <p className="font-medium dark:text-white">
+                Histori Penjualan Produk
+            </p>
+            {loading === "pending" ? (
+                <TransactionSkeleton />
+            ) : transaction?.length === 0 ? (
+                <div className="my-16 w-full">
+                    <Transaction404 />
                 </div>
-            </div>
-            <div className="p-4">
-                <Transaction404 />
-            </div> */}
-        </>
+            ) : (
+                transaction?.map((tx) => (
+                    <div key={tx.id} className="w-full space-y-7">
+                        <div className="flex gap-6 rounded-xl">
+                            <Swiper className="h-14 w-16 rounded-xl object-cover">
+                                {tx.product.pictures.map((picture) => (
+                                    <SwiperSlide key={picture}>
+                                        <img
+                                            className="h-14 w-14 rounded-xl object-cover"
+                                            src={picture}
+                                            alt=""
+                                        />
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                            <div className="w-full space-y-1">
+                                <div className="flex justify-between text-xs text-neutral-03 dark:text-zinc-400">
+                                    <span>Produk Terjual</span>
+                                    <span>
+                                        {dayjs(tx.updatedAt).format(
+                                            "D MMM, HH:mm"
+                                        )}
+                                    </span>
+                                </div>
+                                <div className="dark:text-white">
+                                    {tx?.product.name}
+                                </div>
+                                {tx?.product.price === tx?.price ? (
+                                    <div className="dark:text-white">
+                                        Berhasil terjual{" "}
+                                        {priceFormatter(tx?.product.price)}
+                                    </div>
+                                ) : (
+                                    <>
+                                        <div className="line-through dark:text-white">
+                                            {priceFormatter(tx?.product.price)}
+                                        </div>
+                                        <div className="dark:text-white">
+                                            Berhasil terjual{" "}
+                                            {priceFormatter(tx?.price)}
+                                        </div>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                        <div className="h-px bg-neutral-200 dark:bg-zinc-800"></div>
+                    </div>
+                ))
+            )}
+        </div>
     )
 }
 
--- src/components/Wishlisted.jsx
+++ src/components/Wishlisted.jsx
@@ -1,75 +1,9 @@
-import Wishlisted404 from "./unfound/Wishlisted404"
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-} from "chart.js"
-import { Line } from "react-chartjs-2"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
 import { getWishlistSeller } from "../redux/wishlistSlice"
 import SellerWishlistsCard from "./SellerWishlistsCard"
 import WishlistSkeleton from "./skeletons/WishlistSkeleton"
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement)
-
-// const options = {
-//     responsive: true,
-//     maintainAspectRatio: false,
-//     plugins: {},
-// };
-
-// const userData = [
-//     {
-//         id: 1,
-//         year: 2016,
-//         pendapatanBersih: 80000,
-//         produkTerjual: 823,
-//     },
-//     {
-//         id: 2,
-//         year: 2017,
-//         pendapatanBersih: 45677,
-//         produkTerjual: 345,
-//     },
-//     {
-//         id: 3,
-//         year: 2018,
-//         pendapatanBersih: 78888,
-//         produkTerjual: 555,
-//     },
-//     {
-//         id: 4,
-//         year: 2019,
-//         pendapatanBersih: 90000,
-//         produkTerjual: 4555,
-//     },
-//     {
-//         id: 5,
-//         year: 2020,
-//         pendapatanBersih: 4300,
-//         produkTerjual: 234,
-//     },
-// ];
-
-// const data = {
-//     labels: userData.map((data) => data.year),
-//     datasets: [
-//         {
-//             label: "Produk Diminati",
-//             data: userData.map((data) => data.pendapatanBersih),
-//             borderColor: "rgb(255, 99, 132)",
-//             backgroundColor: "rgba(255, 99, 132, 0.5)",
-//         },
-//         // {
-//         //     label: "Dataset 2",
-//         //     data: userData.map((data) => data.produkTerjual),
-//         //     borderColor: "rgb(53, 162, 235)",
-//         //     backgroundColor: "rgba(53, 162, 235, 0.5)",
-//         // },
-//     ],
-// };
+import Wishlisted404 from "./unfound/Wishlisted404"
 
 const Wishlisted = () => {
     const dispatch = useDispatch()
@@ -114,18 +48,6 @@ const Wishlisted = () => {
                     ))
                 )}
             </div>
-            {/* <div className="space-y-4 rounded-2xl border border-neutral-200 p-4 shadow-md">
-                <div className="font-medium">Statisti Tokomu</div>
-
-                <div className="w-fit rounded-md p-4 shadow">
-                    <div>Produk Diminati</div>
-                    <div>0</div>
-                    <div>0% dari 7 hari terakhir</div>
-                </div>
-                <div className="h-48 w-full">
-                    <Line options={options} data={data} />
-                </div>
-            </div> */}
         </div>
     )
 }

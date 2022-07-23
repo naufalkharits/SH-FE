13fd4363f436fd4c3c395d848744528acfd8c236
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-23 23:07:18 +0700

    Pagination Fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import {
@@ -7,6 +7,7 @@ import {
     offsetIncrement,
     offsetDecrement,
     resetOffset,
+    setOffset,
 } from "../redux/productsSlice"
 import Hero from "../components/Hero"
 import Category from "../components/Category"
@@ -23,19 +24,23 @@ const Home = () => {
     const products = useSelector(productsSelectors.selectAll)
     const { keyword, offset, loading } = useSelector((state) => state.products)
     const { category } = useSelector((state) => state.categories)
+    const [limit] = useState(10)
 
-    const limit = 10
+    if (searchParams.get("page")) {
+        if (Number(searchParams.get("page")) <= 1) {
+            setSearchParams()
+            dispatch(resetOffset())
+        }
+        if (Number(searchParams.get("page")) * 10 - 10 !== offset) {
+            dispatch(setOffset(Number(searchParams.get("page"))))
+        }
+    } else {
+        if (Number(searchParams.get("page")) * 10 - 10 !== offset) {
+            dispatch(resetOffset())
+        }
+    }
 
     useEffect(() => {
-        if (searchParams.get("page")) {
-            if (
-                Number(searchParams.get("page")) === 1 ||
-                Number(searchParams.get("page")) <= 0
-            ) {
-                setSearchParams()
-                dispatch(resetOffset())
-            }
-        }
         dispatch(
             getProducts({
                 keyword,
@@ -55,20 +60,20 @@ const Home = () => {
                 <div className="container mx-auto flex items-center justify-center gap-4">
                     <button
                         className={classNameJoin(
-                            Number(searchParams.get("page")) === 1 ||
+                            searchParams.get("page") === "1" ||
                                 Number(searchParams.get("page")) <= 0
                                 ? "text-gray dark:text-zinc-700"
                                 : "hover:bg-gray dark:hover:bg-zinc-800",
                             "rounded-md p-1 shadow dark:text-white dark:shadow-zinc-800"
                         )}
                         disabled={
-                            Number(searchParams.get("page")) === 1 ||
-                                Number(searchParams.get("page")) <= 0
+                            searchParams.get("page") === "1" ||
+                            Number(searchParams.get("page")) <= 0
                                 ? true
                                 : false
                         }
                         onClick={() => {
-                            if (Number(searchParams.get("page")) === 2) {
+                            if (searchParams.get("page") === "2") {
                                 setSearchParams()
                                 dispatch(resetOffset())
                             } else {
@@ -91,12 +96,12 @@ const Home = () => {
                         )}
                         disabled={
                             products.length !== 0 &&
-                                products.length % limit === 0
+                            products.length % limit === 0
                                 ? false
                                 : true
                         }
                         onClick={() => {
-                            if (Number(searchParams.get("page")) === 0) {
+                            if (!searchParams.get("page")) {
                                 setSearchParams({
                                     page: 2,
                                 })

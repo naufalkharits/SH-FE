92c9c66d63070fac03a556e83809b447deaadda3
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-07-24 17:05:02 +0700

    Add More Testing

--- src/tests/Register.test.js
+++ src/tests/Register.test.js
@@ -1,95 +1,100 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
-import { GoogleOAuthProvider } from "@react-oauth/google"
 import { store } from "../redux/store"
-import Login from "../pages/Login"
+import Register from "../pages/Register"
 
 const rndr = (outlet) =>
     render(
         <Provider store={store}>
-            <BrowserRouter>
-                <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-                    {outlet}
-                </GoogleOAuthProvider>
-            </BrowserRouter>
+            <BrowserRouter>{outlet}</BrowserRouter>
         </Provider>
     )
 
-test("Email and password value should be empty", () => {
-    rndr(<Login />)
+test("Name, email and password value should be empty", () => {
+    rndr(<Register />)
 
+    const nameInputEl = screen.getByPlaceholderText("Nama Lengkap")
     const emailInputEl = screen.getByPlaceholderText(
         "Contoh: johndee@gmail.com"
     )
     const passwordInputEl = screen.getByPlaceholderText("Masukkan password")
 
+    expect(nameInputEl).toHaveValue("")
     expect(emailInputEl).toHaveValue("")
     expect(passwordInputEl).toHaveValue("")
 })
 
-test("Email and password value on change should not be empty", () => {
-    rndr(<Login />)
+test("Name, email and password value on change should not be empty", () => {
+    rndr(<Register />)
 
+    const nameInputEl = screen.getByPlaceholderText("Nama Lengkap")
     const emailInputEl = screen.getByPlaceholderText(
         "Contoh: johndee@gmail.com"
     )
     const passwordInputEl = screen.getByPlaceholderText("Masukkan password")
 
+    const nameValue = "secondhand"
     const emailValue = "secondhand@email.com"
     const passwordValue = "sh0123456789"
 
+    fireEvent.change(nameInputEl, { target: { value: nameValue } })
     fireEvent.change(emailInputEl, { target: { value: emailValue } })
     fireEvent.change(passwordInputEl, { target: { value: passwordValue } })
 
+    expect(nameInputEl).toHaveValue(nameValue)
     expect(emailInputEl).toHaveValue(emailValue)
     expect(passwordInputEl).toHaveValue(passwordValue)
 })
 
-test("Login button should be disabled when inputs is empty", () => {
-    rndr(<Login />)
+test("Register button should be disabled when inputs is empty", () => {
+    rndr(<Register />)
 
-    const buttonElement = screen.getByRole("button", {
-        name: "Masuk",
-    })
+    const buttonElement = screen.getByRole("button")
 
     expect(buttonElement).toBeDisabled()
 })
 
-test("Login button should not be disabled when inputs is not empty", () => {
-    rndr(<Login />)
+test("Register button should be enabled when inputs is not empty", () => {
+    rndr(<Register />)
 
+    const nameInputEl = screen.getByPlaceholderText("Nama Lengkap")
     const emailInputEl = screen.getByPlaceholderText(
         "Contoh: johndee@gmail.com"
     )
     const passwordInputEl = screen.getByPlaceholderText("Masukkan password")
     const buttonElement = screen.getByRole("button", {
-        name: "Masuk",
+        name: "Daftar",
     })
 
+    const nameValue = "secondhand"
     const emailValue = "secondhand@email.com"
     const passwordValue = "sh0123456789"
 
+    fireEvent.change(nameInputEl, { target: { value: nameValue } })
     fireEvent.change(emailInputEl, { target: { value: emailValue } })
     fireEvent.change(passwordInputEl, { target: { value: passwordValue } })
 
-    expect(buttonElement).not.toBeDisabled()
+    expect(buttonElement).toBeEnabled()
 })
 
 test("Loading should be true when fetching API, and should be false after it", async () => {
-    rndr(<Login />)
+    rndr(<Register />)
 
+    const nameInputEl = screen.getByPlaceholderText("Nama Lengkap")
     const emailInputEl = screen.getByPlaceholderText(
         "Contoh: johndee@gmail.com"
     )
     const passwordInputEl = screen.getByPlaceholderText("Masukkan password")
     const buttonElement = screen.getByRole("button", {
-        name: "Masuk",
+        name: "Daftar",
     })
 
+    const nameValue = "secondhand"
     const emailValue = "secondhand@email.com"
     const passwordValue = "sh0123456789"
 
+    fireEvent.change(nameInputEl, { target: { value: nameValue } })
     fireEvent.change(emailInputEl, { target: { value: emailValue } })
     fireEvent.change(passwordInputEl, { target: { value: passwordValue } })
     fireEvent.click(buttonElement)

e0151d3e23da4ea3409b0acede1c5be40e66a672
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-10-10 20:31:57 +0700

    Minor Fixes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,19 @@
 importScripts("https://www.gstatic.com/firebasejs/9.9.0/firebase-app-compat.js")
-importScripts(
-    "https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-compat.js"
-)
+importScripts("https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-compat.js")
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
 const messaging = firebase.messaging()
 
-messaging.onBackgroundMessage((payload) => {})
+messaging.onBackgroundMessage((payload) => {
+  return self.registration.showNotification("test")
+})

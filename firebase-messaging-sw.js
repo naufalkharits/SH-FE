9efe26190d24b6cbd62ade300ed80f6acf48d175
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-06 12:12:34 +0700

    comment code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 const messaging = firebase.messaging()
 
-messaging.onBackgroundMessage((payload) => {
-  return self.registration.showNotification("test")
-})
+// messaging.onBackgroundMessage((payload) => {
+//   return self.registration.showNotification("test")
+// })

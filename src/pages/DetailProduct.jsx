77532f0b16695fdb46e16f32ecb6dba146f31db9
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-17 18:29:35 +0700

    refactor: create sold out floating button
    component

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -343,14 +343,7 @@ const DetailProduct = () => {
                         IN ORDER
                       </button>
                     ) : product?.status === "SOLD" ? (
-                      <button
-                        className={classNameJoin(
-                          isModalOn ? "hidden" : "sm:hidden",
-                          "fixed inset-x-0 bottom-8 z-50 mx-auto w-fit rounded-2xl bg-neutral-02 py-3.5 px-6 text-sm text-white dark:bg-zinc-500 sm:block"
-                        )}
-                        disabled>
-                        SOLD OUT
-                      </button>
+                      <SoldOutFloatingButton />
                     ) : (
                       <button
                         className={classNameJoin(
